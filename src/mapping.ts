d1369691274dfa4349368cede78296d44d5678a5
Gabriel Nicolás González <gabriel@camba.coop>
2021-01-15 16:42:21 -0300

    removed example entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,65 +3,65 @@ import {
   Execute,
   Vote,
 } from "../generated/HumanityGovernance/HumanityGovernance";
-import { ExampleEntity, Proposal, GlobalVotes } from "../generated/schema";
+import { Proposal, GlobalVotes } from "../generated/schema";
 let ZERO = BigInt.fromI32(0)
 let ONE = BigInt.fromI32(1)
 
-export function handleExecute(event: Execute): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex());
+// export function handleexecute(event: execute): void {
+//   // entities can be loaded from the store using a string id; this id
+//   // needs to be unique across all entities of the same type
+//   let entity = exampleentity.load(event.transaction.from.tohex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex());
+//   // entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (entity == null) {
+//     entity = new exampleentity(event.transaction.from.tohex());
 
-    // Entity fields can be set using simple assignments
-    entity.count = ZERO
-  }
+//     // entity fields can be set using simple assignments
+//     entity.count = zero
+//   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count.plus(ONE);
+//   // bigint and bigdecimal math are supported
+//   entity.count = entity.count.plus(one);
 
-  // Entity fields can be set based on event parameters
-  entity.proposalId = event.params.proposalId;
+//   // entity fields can be set based on event parameters
+//   entity.proposalid = event.params.proposalid;
 
-  // Entities can be written to the store with `.save()`
-  entity.save();
+//   // entities can be written to the store with `.save()`
+//   entity.save();
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+//   // note: if a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. instead, create it fresh with
+//   // `new entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.proposals(...)
-  // - contract.yesVotes(...)
-  // - contract.time(...)
-  // - contract.OPEN_VOTE_PERIOD(...)
-  // - contract.TOTAL_VOTE_PERIOD(...)
-  // - contract.getProposalsCount(...)
-  // - contract.propose(...)
-  // - contract.void(...)
-  // - contract.withdrawTimes(...)
-  // - contract.proposalFee(...)
-  // - contract.getProposal(...)
-  // - contract.noVotes(...)
-  // - contract.proposeWithFeeRecipient(...)
-  // - contract.VETO_PERIOD(...)
-  // - contract.token(...)
-  // - contract.deposits(...)
-}
+//   // it is also possible to access smart contracts from mappings. for
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = contract.bind(event.address)
+//   //
+//   // the following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.proposals(...)
+//   // - contract.yesvotes(...)
+//   // - contract.time(...)
+//   // - contract.open_vote_period(...)
+//   // - contract.total_vote_period(...)
+//   // - contract.getproposalscount(...)
+//   // - contract.propose(...)
+//   // - contract.void(...)
+//   // - contract.withdrawtimes(...)
+//   // - contract.proposalfee(...)
+//   // - contract.getproposal(...)
+//   // - contract.novotes(...)
+//   // - contract.proposewithfeerecipient(...)
+//   // - contract.veto_period(...)
+//   // - contract.token(...)
+//   // - contract.deposits(...)
+// }
 
 export function handleVote(event: Vote): void {
   let proposalId = event.params.proposalId.toHex();
